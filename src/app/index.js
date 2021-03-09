1492930fd25ebafa763106a54854dd44ae9ad3bf
sigecuqui <sigita.guogaite@gmail.com>
2021-03-09 18:08:02 +0200

    add theme swither

--- src/app/index.js
+++ src/app/index.js
@@ -8,13 +8,29 @@ import store from "./store";
 import { Layout, PrivateRoute, PublicRoute, ScrollToTop } from "./components";
 import { Home, Login, Content, SingleContentEntry, SignUp } from "./pages";
 
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import React, { useState } from 'react';
+
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }
 
   return (
     <Provider store={store}>
       <Router>
-      <ScrollToTop />
+        <ScrollToTop />
         <Layout>
+       <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <GlobalStyles />
+        <button className="toggle-button" onClick={toggleTheme}>🌗</button>
           <Switch>
             <PublicRoute exact path="/" component={Home} />
             <Route exact path="/login">
@@ -30,6 +46,7 @@ function App() {
               <SignUp />
             </Route>
           </Switch>
+           </ThemeProvider>
         </Layout>
       </Router>
     </Provider>
---
+++ src/app/theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  body: 'white',
+  text: 'black',
+  cardBackground: 'white'
+}
+
+export const darkTheme = {
+  body: 'black',
+  text: '#FAFAFA',
+  cardBackground: '#222222'
+}
